7fd6f27b223183ac51a48cb986e3f39077f04334
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2014-11-07 15:48:37 +0100

    adjusted commons-net lib

--- generateData/UpdateGOdata.sh
+++ generateData/UpdateGOdata.sh
@@ -7,7 +7,7 @@ DBpath=$( echo "ReadProgram('lib/darwinit'): eval( symbol(lowercase('$dataset').
 BINDIR="$DARWIN_BROWSER_REPO_PATH/data/UpdateGO/bin"
 LIBDIR="$DARWIN_BROWSER_REPO_PATH/data/UpdateGO/lib"
 
-java -cp $BINDIR:$LIBDIR/mysql-connector-java-5.1.13-bin.jar:$LIBDIR/commons-net-2.0.jar ch.ethz.cbrg.UpdateGO $DBpath
+java -cp $BINDIR:$LIBDIR/mysql-connector-java-5.1.13-bin.jar:$LIBDIR/commons-net-3.1.jar ch.ethz.cbrg.UpdateGO $DBpath
 
 
 
