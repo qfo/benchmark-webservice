b69f12a00e19de7ba02237db891a403b1c620209
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2019-02-27 15:14:16 +0100

    script to create and populate refset volumes

---
+++ build_refset_volumes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+type -a docker > /dev/null
+
+if [ $? -ne 0 ] ; then
+	echo "UNCONFIGURED: No docker executable" 1>&2
+	exit 1
+fi
+
+for year in 2011 2018 ; do
+    VOL="refset$year"
+    docker volume create $VOL
+    docker run --rm -t -i --mount source=$VOL,target=/refset qfo_darwin:latest bash -c "cd /refset;
+        curl -SL -O https://orthology.benchmarkservice.org/refsets/$year/Summaries.drw.gz;
+        curl -SL -O https://orthology.benchmarkservice.org/refsets/$year/GOdata.drw.gz
+        curl -SL -O https://orthology.benchmarkservice.org/refsets/$year/ServerIndexed.db
+        curl -SL -O https://orthology.benchmarkservice.org/refsets/$year/ServerIndexed.db.map
+        curl -SL -O https://orthology.benchmarkservice.org/refsets/$year/ServerIndexed.db.tree"
+done
+
+
+
+
+
