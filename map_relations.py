081c650ed67d2fe7a6f3fd80f218aa55f6781db9
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2021-10-19 12:11:32 +0200

    refactor some shared functions into helper.py module

--- map_relations.py
+++ map_relations.py
@@ -10,63 +10,16 @@ try:
     import lxml.etree as etree
 except ImportError:
     import xml.etree.ElementTree as etree
-import os
 import re
-from io import BytesIO
-import bz2
-import gzip
 import itertools
 import sqlite3
 import logging
+from helpers import auto_open, unique, load_json_file
 logger = logging.getLogger("relations-processor")
-
-
-# File opening. This is based on the example on SO here:
-# http://stackoverflow.com/a/26986344
-fmagic = {b'\x1f\x8b\x08': gzip.open,
-          b'\x42\x5a\x68': bz2.BZ2File}
 
 
 # uniprot accession regex: https://www.uniprot.org/help/accession_numbers
 RE_UP = re.compile(r"^[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}$")
-
-
-def auto_open(fn, *args, **kwargs):
-    """function to open regular or compressed files for read / write.
-
-    This function opens files based on their "magic bytes". Supports bz2
-    and gzip. If it finds neither of these, presumption is it is a
-    standard, uncompressed file.
-
-    Example::
-
-        with auto_open("/path/to/file/maybe/compressed", mode="rb") as fh:
-            fh.read()
-
-        with auto_open("/tmp/test.txt.gz", mode="wb") as fh:
-            fh.write("my big testfile")
-
-    :param fn: either a string of an existing or new file path, or
-        a BytesIO handle
-    :param **kwargs: additional arguments that are understood by the
-        underlying open handler
-    :returns: a file handler
-    """
-    if isinstance(fn, BytesIO):
-        return fn
-
-    if os.path.isfile(fn) and os.stat(fn).st_size > 0:
-        with open(fn, 'rb') as fp:
-            fs = fp.read(max([len(x) for x in fmagic]))
-        for (magic, _open) in fmagic.items():
-            if fs.startswith(magic):
-                return _open(fn, *args, **kwargs)
-    else:
-        if fn.endswith('gz'):
-            return gzip.open(fn, *args, **kwargs)
-        elif fn.endswith('bz2'):
-            return bz2.BZ2File(fn, *args, **kwargs)
-    return open(fn, *args, **kwargs)
 
 
 def encode_nr_as_seq(nr):
@@ -84,18 +37,7 @@ def encode_nr_as_seq(nr):
 
 
 def load_mapping(path):
-    with auto_open(path, 'rb') as fh:
-        data = json.loads(fh.read().decode('utf-8'))
-    return data
-
-
-def unique(seq):
-    """Return the elements of a list uniquely while preserving the order
-
-    :param list seq: a list of hashable elements
-    :returns: new list with first occurence of elements of seq"""
-    seen = set()
-    return [x for x in seq if x not in seen and not seen.add(x)]
+    return load_json_file(path)
 
 
 class PairwiseOrthologRelationExtractor(object):
--- validate.py
+++ validate.py
@@ -10,58 +10,11 @@ try:
     import lxml.etree as etree
 except ImportError:
     import xml.etree.ElementTree as etree
-import os
-from io import BytesIO
-import bz2
-import gzip
 import logging
 import JSON_templates
+from helpers import auto_open
 
 logger = logging.getLogger("validator")
-
-
-# File opening. This is based on the example on SO here:
-# http://stackoverflow.com/a/26986344
-fmagic = {b'\x1f\x8b\x08': gzip.open,
-          b'\x42\x5a\x68': bz2.BZ2File}
-
-
-def auto_open(fn, *args, **kwargs):
-    """function to open regular or compressed files for read / write.
-
-    This function opens files based on their "magic bytes". Supports bz2
-    and gzip. If it finds neither of these, presumption is it is a
-    standard, uncompressed file.
-
-    Example::
-
-        with auto_open("/path/to/file/maybe/compressed", mode="rb") as fh:
-            fh.read()
-
-        with auto_open("/tmp/test.txt.gz", mode="wb") as fh:
-            fh.write("my big testfile")
-
-    :param fn: either a string of an existing or new file path, or
-        a BytesIO handle
-    :param **kwargs: additional arguments that are understood by the
-        underlying open handler
-    :returns: a file handler
-    """
-    if isinstance(fn, BytesIO):
-        return fn
-
-    if os.path.isfile(fn) and os.stat(fn).st_size > 0:
-        with open(fn, 'rb') as fp:
-            fs = fp.read(max([len(x) for x in fmagic]))
-        for (magic, _open) in fmagic.items():
-            if fs.startswith(magic):
-                return _open(fn, *args, **kwargs)
-    else:
-        if fn.endswith('gz'):
-            return gzip.open(fn, *args, **kwargs)
-        elif fn.endswith('bz2'):
-            return bz2.BZ2File(fn, *args, **kwargs)
-    return open(fn, *args, **kwargs)
 
 
 def load_mapping(path):
