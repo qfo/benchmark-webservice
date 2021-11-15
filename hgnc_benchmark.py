13c37782d9db97d8fef700d3e6371f3bea066ff1
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2021-11-15 12:14:17 +0100

    adding precision metric to hgnc benchmark

--- hgnc_benchmark.py
+++ hgnc_benchmark.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import collections
-import io
 import itertools
 import json
 import logging
@@ -8,18 +7,14 @@ import math
 import os
 import sqlite3
 
-import Bio.Phylo
-import dendropy
-
 from JSON_templates import write_assessment_dataset
-from helpers import auto_open, load_json_file
+from helpers import auto_open
 
 logger = logging.getLogger("HGNC-Benchmark")
 Protein = collections.namedtuple("Protein", ["Acc", "Species"])
 
 
 def compute_hgnc_benchmark(hgnc_orthologs, db_path, raw_out):
-
     def get_prot_data_for(proteins):
         data = {}
         cur = con.cursor()
@@ -29,6 +24,20 @@ def compute_hgnc_benchmark(hgnc_orthologs, db_path, raw_out):
         for row in cur.fetchall():
             data[row[0]] = Protein(row[1], row[2])
         return data
+
+    def get_orthologs_among_subset_of_proteins(proteins):
+        placeholder = ",".join("?" * len(proteins))
+        query = f"SELECT * FROM orthologs WHERE prot_nr1 == ? AND prot_nr2 IN ({placeholder}) AND prot_nr1 < prot_nr2"
+
+        def get_orthologs_for_query(query_protein):
+            cur = con.cursor()
+            args = [query_protein]
+            args.extend(proteins)
+            cur.execute(query, args)
+            return cur.fetchall()
+
+        for p in proteins:
+            yield from get_orthologs_for_query(p)
 
     def get_existing_orthologs(all_orthologs):
         def get_chunk(orthologs):
@@ -42,17 +51,15 @@ def compute_hgnc_benchmark(hgnc_orthologs, db_path, raw_out):
         cur = con.cursor()
         res = []
         for chunk in range(0, len(all_orthologs), 500):
-            res.extend(get_chunk(all_orthologs[chunk:chunk+500]))
+            res.extend(get_chunk(all_orthologs[chunk:chunk + 500]))
         return res
 
     def write_raw_rels(out, rels, typ):
         for en1, en2 in rels:
-            out.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(protein_infos[en1].Acc,
-                                                        protein_infos[en2].Acc,
-                                                        typ,
-                                                        hgnc_orthologs[(en1, en2)],
-                                                        protein_infos[en1].Species,
-                                                        protein_infos[en2].Species))
+            out.write(
+                f"{protein_infos[en1].Acc}\t{protein_infos[en2].Acc}\t{typ}\t"
+                f"{hgnc_fam[en1]}\t{hgnc_fam[en2]}\t{protein_infos[en1].Species}\t"
+                f"{protein_infos[en2].Species}\n")
 
     con = sqlite3.connect(db_path)
     nr_true = len(hgnc_orthologs)
@@ -61,33 +68,43 @@ def compute_hgnc_benchmark(hgnc_orthologs, db_path, raw_out):
     hgnc_genes = list(set(itertools.chain.from_iterable(hgnc_orthologs.keys())))
     protein_infos = get_prot_data_for(hgnc_genes)
 
-    all_predicted_orthologs_among_hgnc_genes = get_existing_orthologs(list(hgnc_orthologs.keys()))
+    all_predicted_orthologs_among_hgnc_genes = set(get_orthologs_among_subset_of_proteins(hgnc_genes))
     logger.info(f"Method predicted {len(all_predicted_orthologs_among_hgnc_genes)} among the set of "
                 f"{len(hgnc_genes)} genes in the HGNC dataset")
     missing_true_orthologs -= set((x[0], x[1]) for x in all_predicted_orthologs_among_hgnc_genes)
     logger.info(f"Method didn't predict {len(missing_true_orthologs)} asserted orthologs")
     true_positives = set(hgnc_orthologs.keys()) - missing_true_orthologs
     tp = len(hgnc_orthologs) - len(missing_true_orthologs)
+    tpr = tp / nr_true
+
+    hgnc_fam = {}
+    for (p1, p2), fam in hgnc_orthologs.items():
+        hgnc_fam[p1] = fam
+        hgnc_fam[p2] = fam
+    false_positives = [rel for rel in all_predicted_orthologs_among_hgnc_genes if hgnc_fam[rel[0]] != hgnc_fam[rel[1]]]
+    nr_pos = tp + len(false_positives)
+    ppv = tp / nr_pos
 
     write_raw_rels(raw_out, true_positives, "TP")
+    write_raw_rels(raw_out, false_positives, "FP")
     write_raw_rels(raw_out, missing_true_orthologs, "FN")
-    tpr = tp / nr_true
     logger.info("TPR: {}; nr_true: {}".format(tpr, nr_true))
     metrics = [{"name": "TP", 'value': tp},
                {"name": "FN", "value": len(missing_true_orthologs)},
                {"name": "TPR", "value": tpr, "stderr": 1.96 * math.sqrt(tpr * (1 - tpr) / nr_true)},
-              ]
+               {"name": "PPV", "value": ppv, "stderr": 1.96 * math.sqrt(ppv * (1 - ppv) / nr_pos)},
+               ]
     return metrics
 
 
-def get_hgnc_orthologs(hgnc_orthologs):
+def get_hgnc_orthologs(hgnc_orthologs_fname):
     orthologs = {}
-    with auto_open(hgnc_orthologs, 'rt') as fh:
+    with auto_open(hgnc_orthologs_fname, 'rt') as fh:
         for line in fh:
             a, b, fam = line.strip().split("\t")
             a = int(a)
             b = int(b)
-            orthologs[(min(a,b), max(a,b))] = fam
+            orthologs[(min(a, b), max(a, b))] = fam
     return orthologs
 
 
@@ -111,7 +128,7 @@ if __name__ == "__main__":
     parser.add_argument('--outdir', required=True, help="Folder to store the raw output file in")
     parser.add_argument('--db', required=True, help="Path to sqlite database with pairwise predictions")
     parser.add_argument('--com', required=True, help="community id")
-    parser.add_argument('--hgnc-orthologs', required=True, help="Path to textfile with HGNC asserted orthologs")
+    parser.add_argument('--hgnc-orthologs', required=True, help="Path to text file with HGNC asserted orthologs")
     parser.add_argument('--participant', required=True, help="Name of participant method")
     parser.add_argument('--log', help="Path to log file. Defaults to stderr")
     parser.add_argument('-d', '--debug', action="store_true", help="Set logging to debug level")
