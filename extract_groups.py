f65bc0072a3d7a2e9e62cd75e98d407e3ef6ee8f
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2021-11-18 23:58:11 +0100

    deal with skip-species in group extraction

--- extract_groups.py
+++ extract_groups.py
@@ -14,67 +14,10 @@ import Bio.Phylo
 import bz2
 import gzip
 import itertools
+from helpers import auto_open, load_json_file
+
 import logging
 logger = logging.getLogger("relations-processor")
-
-
-# File opening. This is based on the example on SO here:
-# http://stackoverflow.com/a/26986344
-fmagic = {b'\x1f\x8b\x08': gzip.open,
-          b'\x42\x5a\x68': bz2.BZ2File}
-
-
-def auto_open(fn, *args, **kwargs):
-    """function to open regular or compressed files for read / write.
-
-    This function opens files based on their "magic bytes". Supports bz2
-    and gzip. If it finds neither of these, presumption is it is a
-    standard, uncompressed file.
-
-    Example::
-
-        with auto_open("/path/to/file/maybe/compressed", mode="rb") as fh:
-            fh.read()
-
-        with auto_open("/tmp/test.txt.gz", mode="wb") as fh:
-            fh.write("my big testfile")
-
-    :param fn: either a string of an existing or new file path, or
-        a BytesIO handle
-    :param **kwargs: additional arguments that are understood by the
-        underlying open handler
-    :returns: a file handler
-    """
-    if isinstance(fn, BytesIO):
-        return fn
-
-    if os.path.isfile(fn) and os.stat(fn).st_size > 0:
-        with open(fn, 'rb') as fp:
-            fs = fp.read(max([len(x) for x in fmagic]))
-        for (magic, _open) in fmagic.items():
-            if fs.startswith(magic):
-                return _open(fn, *args, **kwargs)
-    else:
-        if fn.endswith('gz'):
-            return gzip.open(fn, *args, **kwargs)
-        elif fn.endswith('bz2'):
-            return bz2.BZ2File(fn, *args, **kwargs)
-    return open(fn, *args, **kwargs)
-
-
-def load_mapping(path):
-    with auto_open(path, 'rb') as fh:
-        data = json.loads(fh.read().decode('utf-8'))
-    return data
-
-
-def unique(seq):
-    """Return the elements of a list uniquely while preserving the order
-
-    :param list seq: a list of hashable elements
-    :returns: new list with first occurence of elements of seq"""
-    seen = set()
-    return [x for x in seq if x not in seen and not seen.add(x)]
 
 
 class Clade:
@@ -101,6 +44,7 @@ class GroupExtractor(object):
         self.valid_id_map = mapping_data['mapping']
         self.internal_genome_offs = mapping_data['Goff']
         self.species_order = mapping_data['species']
+        self.excluded_ids = mapping_data['excluded_ids'] if 'excluded_ids' in mapping_data else set([])
         self.processed_stats = {'last': time(), 'processed_toplevel': 0}
         self.target_clade = target_clade
         self.genes = {}
@@ -134,7 +78,11 @@ class GroupExtractor(object):
         genes = []
         for child in node.iter():
             if child.tag == "{http://orthoXML.org/2011/}geneRef":
-                genes.append(self.genes[child.get('id')])
+                try:
+                    genes.append(self.genes[child.get('id')])
+                except KeyError:
+                    logger.info(f"ignoring gene(id={child.get('id')}), probably in skip set.")
+                    pass
             elif child.tag == "{http://orthoXML.org/2011/}orthologGroup":
                 genes.extend((n for n in child.text if isinstance(n, Gene)))
         return genes
@@ -238,7 +186,7 @@ if __name__ == "__main__":
     logging.basicConfig(**log_conf)
 
     clade = Clade(conf.taxonomy, conf.clade)
-    mapping_data = load_mapping(conf.mapping)
+    mapping_data = load_json_file(conf.mapping)
     processor = GroupExtractor(mapping_data, clade)
 
     with open(conf.out, 'w') as fh:
